a203aa8531931fc75dfe0a3d09525667ec11bd64
Rishi Raj <rraj7@uic.edu>
2020-07-07 19:59:56 -0500

    Initial Commit

---
+++ code.py
@@ -0,0 +1,10 @@
+#import tensorflow as tf;
+#from tensorflow import keras;
+#from tensorflow.keras.preprocessing.text import Tokenizer;
+
+import pandas as pd;
+import numpy as np;
+
+data = pd.read_csv('halamka_posts_1836.tsv', sel = '\t');
+data();
+
